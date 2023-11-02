35fc3772e2dbd0a3e06fb50251cd3665bd99238c
Abu Zafar Emon <zafar emon>
2023-11-02 21:44:00 +0600

    grid layout init

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,21 @@
 import Header from "./Header";
+import images from "../data/imagesData";
+import { useState } from "react";
 
 const Gallery = () => {
+  const [thumbnails, setThumbnails] = useState(images);
   return (
     <main className="bg-white rounded-md">
       <div className="container">
         <Header />
         <hr />
-        <section className="p-6"></section>
+        <section className="h-full w-full p-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+            {thumbnails.map((image, index) => (
+              <img key={index} src={image.thumbnail} alt="" />
+            ))}
+          </div>
+        </section>
       </div>
     </main>
   );
