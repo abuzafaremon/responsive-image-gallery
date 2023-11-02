20cfb9380ec0de53447c6a028eb902cdb22ed9d0
Abu Zafar Emon <zafar emon>
2023-11-02 20:06:32 +0600

    create mock data from given images

---
+++ src/data/imagesData.js
@@ -0,0 +1,15 @@
+const images = [
+  { id: 1, thumbnail: "/assets/images/image-1.webp" },
+  { id: 2, thumbnail: "/assets/images/image-2.webp" },
+  { id: 3, thumbnail: "/assets/images/image-3.webp" },
+  { id: 4, thumbnail: "/assets/images/image-4.webp" },
+  { id: 5, thumbnail: "/assets/images/image-5.webp" },
+  { id: 6, thumbnail: "/assets/images/image-6.webp" },
+  { id: 7, thumbnail: "/assets/images/image-7.webp" },
+  { id: 8, thumbnail: "/assets/images/image-8.webp" },
+  { id: 9, thumbnail: "/assets/images/image-9.webp" },
+  { id: 10, thumbnail: "/assets/images/image-10.jpeg" },
+  { id: 11, thumbnail: "/assets/images/image-11.jpeg" },
+];
+
+export default images;
